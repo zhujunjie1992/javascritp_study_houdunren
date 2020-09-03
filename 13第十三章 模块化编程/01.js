d949698bef5710bf2bd0404bfce301ea2662e678
zhujunjie1992 <291245526@qq.com>
2020-09-03 12:32:59 +0800

    1

---
+++ 13第十三章 模块化编程/01.js
@@ -0,0 +1,30 @@
+// let name = "zhujunjie";
+
+// // let show = function(){
+// //   console.log("show方法")
+// // }
+
+// let url = "www.baidu.com";
+
+// export default{
+//   name,
+//   url
+// }
+
+
+// export let name = "zhujunjie";
+
+let name = "zhujunjie";
+let show = function(){
+  console.log(show);
+}
+let url = "www.baidu.com";
+let obj = {
+  name:"obj"
+}
+
+export default{
+  name,
+  url,
+  obj
+}
